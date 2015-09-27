c45a146eebb81c9a2aca54222964f539bd1267d0
jlfaucher <jfaucher.fr@gmail.com>
2015-09-28 01:16:42 +0200

    debug mode : add DEBUG (used by GCI)

--- scripts/setenv-debug.bat
+++ scripts/setenv-debug.bat
@@ -1,6 +1,10 @@
 @echo off
 if defined echo echo %echo%
 
+set CFLAGS=/D_DEBUG /DDEBUG %CFLAGS%
+set CPPFLAGS=/D_DEBUG /DDEBUG %CPPFLAGS%
+set CXXFLAGS=/D_DEBUG /DDEBUG %CXXFLAGS%
+
 :: Old build system (makeorx DEBUG)
 set rel=0
 set dbg=1
