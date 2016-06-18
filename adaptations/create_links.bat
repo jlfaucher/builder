53493780d8de911eab39a2599a7d8b7d4c992695
jlfaucher <jfaucher.fr@gmail.com>
2016-06-18 21:04:59 +0200

    adaptations for main/branches/4.2

--- adaptations/create_links.bat
+++ adaptations/create_links.bat
@@ -17,6 +17,10 @@ set GCI=c:\jlf\local\rexx\GCI
 set BSF4OOREXX=c:\jlf\local\rexx\bsf4oorexx
 set OOREXX=c:\jlf\local\rexx\oorexx
 
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::GCI\gci-sources.1.1
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
 set target=%BUILDER%\adaptations\GCI\gci-source.1.1
 set source=%GCI%\gci-source.1.1
 call :mklink %source%\gci-try.rexx                  %target%\gci-try.rexx
@@ -32,12 +36,49 @@ call :mklink %source%\GNUmakefile-builder           %target%\GNUmakefile-builder
 call :mklink %source%\Makefile-builder.vc           %target%\Makefile-builder.vc
 
 
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: bsf4oorexx\trunk
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
 set target=%BUILDER%\adaptations\bsf4oorexx\trunk\bsf4oorexx.dev\source_cc
 set source=%BSF4OOREXX%\svn\trunk\bsf4oorexx.dev\source_cc
 call :mklink %source%\Makefile-builder              %target%\Makefile-builder
 call :mklink %source%\apple-Makefile-builder.mak    %target%\apple-Makefile-builder.mak
 call :mklink %source%\lin_bsf4rexx-builder.mak      %target%\lin_bsf4rexx-builder.mak
 
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: oorexx\official\main\branches\4.2
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+set target=%BUILDER%\adaptations\oorexx\official\main\branches\4.2\trunk\api\platform\windows
+set source=%OOREXX%\official\main\branches\4.2\trunk\api\platform\windows
+call :mklink %source%\rexxapitypes.h                %target%\rexxapitypes.h
+
+set target=%BUILDER%\adaptations\oorexx\official\main\branches\4.2\trunk
+set source=%OOREXX%\official\main\branches\4.2\trunk
+call :mklink %source%\cl_infos.cpp                  %target%\cl_infos.cpp
+call :mklink %source%\Makefile.am                   %target%\Makefile.am
+call :mklink %source%\makeorx_verbose.bat           %target%\makeorx_verbose.bat
+call :mklink %source%\makeorx.bat                   %target%\makeorx.bat
+call :mklink %source%\orxdb.bat                     %target%\orxdb.bat
+
+set target=%BUILDER%\adaptations\oorexx\official\main\branches\4.2\trunk\interpreter\platform\windows
+set source=%OOREXX%\official\main\branches\4.2\trunk\interpreter\platform\windows
+call :mklink %source%\PlatformDefinitions.h         %target%\PlatformDefinitions.h
+
+set target=%BUILDER%\adaptations\oorexx\official\main\branches\4.2\trunk\lib
+set source=%OOREXX%\official\main\branches\4.2\trunk\lib
+call :mklink %source%\orxwin32.mak                  %target%\orxwin32.mak
+
+set target=%BUILDER%\adaptations\oorexx\official\main\branches\4.2\trunk\platform\windows
+set source=%OOREXX%\official\main\branches\4.2\trunk\platform\windows
+call :mklink %source%\buildorx.bat                  %target%\buildorx.bat
+
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: oorexx\official\main\releases\4.2.0
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 set target=%BUILDER%\adaptations\oorexx\official\main\releases\4.2.0\trunk\api\platform\windows
 set source=%OOREXX%\official\main\releases\4.2.0\trunk\api\platform\windows
@@ -46,6 +87,7 @@ call :mklink %source%\rexxapitypes.h                %target%\rexxapitypes.h
 set target=%BUILDER%\adaptations\oorexx\official\main\releases\4.2.0\trunk
 set source=%OOREXX%\official\main\releases\4.2.0\trunk
 call :mklink %source%\cl_infos.cpp                  %target%\cl_infos.cpp
+call :mklink %source%\Makefile.am                   %target%\Makefile.am
 call :mklink %source%\makeorx_verbose.bat           %target%\makeorx_verbose.bat
 call :mklink %source%\makeorx.bat                   %target%\makeorx.bat
 call :mklink %source%\orxdb.bat                     %target%\orxdb.bat
@@ -62,13 +104,35 @@ set target=%BUILDER%\adaptations\oorexx\official\main\releases\4.2.0\trunk\platf
 set source=%OOREXX%\official\main\releases\4.2.0\trunk\platform\windows
 call :mklink %source%\buildorx.bat                  %target%\buildorx.bat
 
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: oorexx\official\main\trunk
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+set target=%BUILDER%\adaptations\oorexx\official\main\trunk
+set source=%OOREXX%\official\main\trunk
+call :mklink %source%\CMakeLists.txt                %target%\CMakeLists.txt
+
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: oorexx\official\test\branches\4.2.0
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
 set target=%BUILDER%\adaptations\oorexx\official\test\branches\4.2.0\trunk\external\API
 set source=%OOREXX%\official\test\branches\4.2.0\trunk\external\API
 call :mklink %source%\Makefile.windows              %target%\Makefile.windows
 
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: oorexx\official\test\trunk
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
 set target=%BUILDER%\adaptations\oorexx\official\test\trunk\external\API
 set source=%OOREXX%\official\test\trunk\external\API
 call :mklink %source%\Makefile.windows              %target%\Makefile.windows
+
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 echo.
 if "%doit%" == "echo" (
--- adaptations/create_links.sh
+++ adaptations/create_links.sh
@@ -13,6 +13,10 @@ GCI=/local/rexx/GCI
 BSF4OOREXX=/local/rexx/bsf4oorexx
 OOREXX=/local/rexx/oorexx
 
+################################################################################
+# GCI/gci-source.1.1
+################################################################################
+
 source=$BUILDER/adaptations/GCI/gci-source.1.1
 target=$GCI/gci-source.1.1
 $doit ln -f -s $source/gci-try.rexx                  $target/gci-try.rexx
@@ -28,12 +32,49 @@ $doit ln -f -s $source/GNUmakefile-builder           $target/GNUmakefile-builder
 $doit ln -f -s $source/Makefile-builder.vc           $target/Makefile-builder.vc
 
 
+################################################################################
+# bsf4oorexx/trunk
+################################################################################
+
 source=$BUILDER/adaptations/bsf4oorexx/trunk/bsf4oorexx.dev/source_cc
 target=$BSF4OOREXX/svn/trunk/bsf4oorexx.dev/source_cc
 $doit ln -f -s $source/Makefile-builder              $target/Makefile-builder
 $doit ln -f -s $source/apple-Makefile-builder.mak    $target/apple-Makefile-builder.mak
 $doit ln -f -s $source/lin_bsf4rexx-builder.mak      $target/lin_bsf4rexx-builder.mak
 
+
+################################################################################
+# oorexx/official/main/branches/4.2
+################################################################################
+
+source=$BUILDER/adaptations/oorexx/official/main/branches/4.2/trunk/api/platform/windows
+target=$OOREXX/official/main/branches/4.2/trunk/api/platform/windows
+$doit ln -f -s $source/rexxapitypes.h                $target/rexxapitypes.h
+
+source=$BUILDER/adaptations/oorexx/official/main/branches/4.2/trunk
+target=$OOREXX/official/main/branches/4.2/trunk
+$doit ln -f -s $source/cl_infos.cpp                  $target/cl_infos.cpp
+$doit ln -f -s $source/Makefile.am                   $target/Makefile.am
+$doit ln -f -s $source/makeorx_verbose.bat           $target/makeorx_verbose.bat
+$doit ln -f -s $source/makeorx.bat                   $target/makeorx.bat
+$doit ln -f -s $source/orxdb.bat                     $target/orxdb.bat
+
+source=$BUILDER/adaptations/oorexx/official/main/branches/4.2/trunk/interpreter/platform/windows
+target=$OOREXX/official/main/branches/4.2/trunk/interpreter/platform/windows
+$doit ln -f -s $source/PlatformDefinitions.h         $target/PlatformDefinitions.h
+
+source=$BUILDER/adaptations/oorexx/official/main/branches/4.2/trunk/lib
+target=$OOREXX/official/main/branches/4.2/trunk/lib
+$doit ln -f -s $source/orxwin32.mak                  $target/orxwin32.mak
+
+source=$BUILDER/adaptations/oorexx/official/main/branches/4.2/trunk/platform/windows
+target=$OOREXX/official/main/branches/4.2/trunk/platform/windows
+$doit ln -f -s $source/buildorx.bat                  $target/buildorx.bat
+
+
+################################################################################
+# oorexx/official/main/releases/4.2.0
+################################################################################
 
 source=$BUILDER/adaptations/oorexx/official/main/releases/4.2.0/trunk/api/platform/windows
 target=$OOREXX/official/main/releases/4.2.0/trunk/api/platform/windows
@@ -42,6 +83,7 @@ $doit ln -f -s $source/rexxapitypes.h                $target/rexxapitypes.h
 source=$BUILDER/adaptations/oorexx/official/main/releases/4.2.0/trunk
 target=$OOREXX/official/main/releases/4.2.0/trunk
 $doit ln -f -s $source/cl_infos.cpp                  $target/cl_infos.cpp
+$doit ln -f -s $source/Makefile.am                   $target/Makefile.am
 $doit ln -f -s $source/makeorx_verbose.bat           $target/makeorx_verbose.bat
 $doit ln -f -s $source/makeorx.bat                   $target/makeorx.bat
 $doit ln -f -s $source/orxdb.bat                     $target/orxdb.bat
@@ -58,13 +100,35 @@ source=$BUILDER/adaptations/oorexx/official/main/releases/4.2.0/trunk/platform/w
 target=$OOREXX/official/main/releases/4.2.0/trunk/platform/windows
 $doit ln -f -s $source/buildorx.bat                  $target/buildorx.bat
 
+
+################################################################################
+# oorexx/official/main/trunk
+################################################################################
+
+source=$BUILDER/adaptations/oorexx/official/main/trunk
+target=$OOREXX/official/main/trunk
+$doit ln -f -s $source/CMakeLists.txt                $target/CMakeLists.txt
+
+
+################################################################################
+# oorexx/official/test/branches/4.2.0
+################################################################################
+
 source=$BUILDER/adaptations/oorexx/official/test/branches/4.2.0/trunk/external/API
 target=$OOREXX/official/test/branches/4.2.0/trunk/external/API
 $doit ln -f -s $source/Makefile.windows              $target/Makefile.windows
 
+
+################################################################################
+# oorexx/official/test/trunk
+################################################################################
+
 source=$BUILDER/adaptations/oorexx/official/test/trunk/external/API
 target=$OOREXX/official/test/trunk/external/API
 $doit ln -f -s $source/Makefile.windows              $target/Makefile.windows
+
+
+################################################################################
 
 echo
 if [ "$doit" == "echo" ]
