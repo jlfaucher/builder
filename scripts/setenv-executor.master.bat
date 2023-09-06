b009573d9edc160438df4aad213cf6e0c684d653
jlfaucher <jfaucher.fr@gmail.com>
2023-09-06 17:10:18 +0200

    Preprocessor variable STRONG_TYPES no longer used.

--- scripts/setenv-executor.master.bat
+++ scripts/setenv-executor.master.bat
@@ -1,6 +1,7 @@
 @echo off
 if defined echo echo %echo%
 
+:: No longer needed
 :: Pfff... CPPFLAGS ignored by cmake ???
-set CPPFLAGS=/DSTRONG_TYPES %CPPFLAGS%
-set CXXFLAGS=/DSTRONG_TYPES %CXXFLAGS%
+::set CPPFLAGS=/DSTRONG_TYPES %CPPFLAGS%
+::set CXXFLAGS=/DSTRONG_TYPES %CXXFLAGS%
