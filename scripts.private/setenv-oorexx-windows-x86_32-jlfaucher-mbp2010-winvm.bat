4c8e80149276fa2006375ebf9fb57ec86a053bee
jlfaucher <jfaucher.fr@gmail.com>
2025-05-24 00:28:43 +0200

    Upgrade bsf4oorexx-v641 and bsf4oorexx-v850 on my MBP-2010

--- scripts.private/setenv-oorexx-windows-x86_32-jlfaucher-mbp2010-winvm.bat
+++ scripts.private/setenv-oorexx-windows-x86_32-jlfaucher-mbp2010-winvm.bat
@@ -49,21 +49,23 @@ call shellscriptlib :prepend_path PATH "E:\windiff"
 :: cmake
 call shellscriptlib :prepend_path PATH "C:\Program Files\CMake\bin"
 
-:: bsf4oorexx version 641 - all platforms
-if "%builder_target%" == "executor" (
+if "%builder_target%" == "official" if "%builder_src_relative_path%" == "main\trunk" goto :bsf4oorexx_v850
+goto :bsf4oorexx_v641
+:bsf4oorexx_v850
+    :: bsf4oorexx version 850 - all platforms - can't be used by Executor
     set BSF4OOREXX_HOME_DRIVE=%HOST_DRIVE%
-    set BSF4OOREXX_HOME=%HOST_DRIVE%\local\rexx\bsf4oorexx\BSF4ooRexx_install_v641-20220131-ga\bsf4oorexx
-    set BSF4OOREXX_JAR=bsf4ooRexx-v641-20220131-bin.jar
+    set BSF4OOREXX_HOME=%HOST_DRIVE%\local\rexx\bsf4oorexx\BSF4ooRexx_install_v850-20240707-refresh\bsf4oorexx
+    set BSF4OOREXX_JAR=bsf4ooRexx-v850-20240707-bin.jar
     call %builder_scripts_dir%.private\shellscriptlib-windows :declare_bsf4oorexx_distribution
-)
-
-:: bsf4oorexx version 850 - all platforms - can't be used by Executor
-if not "%builder_target%" == "executor" (
+    goto :bsf4oorexx_done
+:bsf4oorexx_v641
+    :: bsf4oorexx version 641 - all platforms
     set BSF4OOREXX_HOME_DRIVE=%HOST_DRIVE%
-    set BSF4OOREXX_HOME=%HOST_DRIVE%\local\rexx\bsf4oorexx\BSF4ooRexx_install_v850-20230718-beta\bsf4oorexx
-    set BSF4OOREXX_JAR=bsf4ooRexx-v850-20230718-bin.jar
+    set BSF4OOREXX_HOME=%HOST_DRIVE%\local\rexx\bsf4oorexx\BSF4ooRexx_install_v641-20221002-refresh\bsf4oorexx
+    set BSF4OOREXX_JAR=bsf4ooRexx-v641-20221002-bin.jar
     call %builder_scripts_dir%.private\shellscriptlib-windows :declare_bsf4oorexx_distribution
-)
+    goto :bsf4oorexx_done
+:bsf4oorexx_done
 
 :: bsf4oorexx portable (641) - windows only
 ::set BSF4OOREXX_HOME_DRIVE=%HOST_DRIVE%
