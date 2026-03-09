419a580bc0e18de6c72c67377bfea77cf157d036
jlfaucher <jfaucher.fr@gmail.com>
2026-03-09 14:00:08 +0100

    New script run_testOORexx to run the test suite with the right [DY]LD_LIBRARY_PATH and PATH. With these settings, the native API tests and the ADDRESS.testGroup are executed with 0 warning/error.

---
+++ scripts/run_testOORexx.bat
@@ -0,0 +1,11 @@
+@echo off
+
+:: Let find the dynamic libraries needed by the testsuite for native_api testing
+:: and ADDRESS.testGroup
+setlocal
+set PATH=%builder_build_dir%/bin;%PATH%
+:: The test suite uses the find utility; make sure that it is the Windows one
+:: and not the GNU one (I have the GNU one...).
+set PATH=C:\WINDOWS\system32;%PATH%
+rexx testOORexx %*
+endlocal
