a0f6b6e9b6606b174bb5729178a0b2645b8566dd
jlfaucher <jfaucher.fr@gmail.com>
2026-05-01 20:28:12 +0200

    Update script run_testOORexx to run the test suite with the right [DY]LD_LIBRARY_PATH and PATH.

--- scripts/run_testOORexx.bat
+++ scripts/run_testOORexx.bat
@@ -1,9 +1,12 @@
 @echo off
 
+:: Let find the executables needed by the testsuite for native_api testing,
+:: ProcessInvocation.testGroup, RexxStart.testGroup, ProcessRexxStart.testGroup.
 :: Let find the dynamic libraries needed by the testsuite for native_api testing
-:: and ADDRESS.testGroup
+:: and ADDRESS.testGroup.
 setlocal
 set PATH=%builder_build_dir%/bin;%PATH%
+
 :: The test suite uses the find utility; make sure that it is the Windows one
 :: and not the GNU one (I have the GNU one...).
 set PATH=C:\WINDOWS\system32;%PATH%
